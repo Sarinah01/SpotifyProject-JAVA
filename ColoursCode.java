85dec879d9170433e996239a35c7c5a24d894a3b
Sarinah <zaidchudiwala@gmail.com>
2025-12-19 22:16:44 +0530

    Add ColoursCode class for ANSI color codes

---
+++ ColoursCode.java
@@ -0,0 +1,57 @@
+package MusicApp1;
+
+public class ColoursCode {
+    // Reset
+    public static final String RESET = "\u001B[0m";  // Text Reset
+
+    // Regular Colors
+    public static final String BLACK = "\u001B[30m";   // BLACK
+    public static final String RED = "\u001B[31m";     // RED
+    public static final String GREEN = "\u001B[32m";   // GREEN
+    public static final String YELLOW = "\u001B[33m";  // YELLOW
+    public static final String BLUE = "\u001B[34m";    // BLUE
+    public static final String PURPLE = "\u001B[35m";  // PURPLE
+    public static final String CYAN = "\u001B[36m";    // CYAN
+    public static final String WHITE = "\u001B[37m";   // WHITE
+
+    // Bold
+    public static final String BLACK_BOLD = "\u001B[1;30m"; // BOLD BLACK
+    public static final String RED_BOLD = "\u001B[1;31m";   // BOLD RED
+    public static final String GREEN_BOLD = "\u001B[1;32m"; // BOLD GREEN
+    public static final String YELLOW_BOLD = "\u001B[1;33m";// BOLD YELLOW
+    public static final String BLUE_BOLD = "\u001B[1;34m";  // BOLD BLUE
+    public static final String PURPLE_BOLD = "\u001B[1;35m";// BOLD PURPLE
+    public static final String CYAN_BOLD = "\u001B[1;36m";  // BOLD CYAN
+    public static final String WHITE_BOLD = "\u001B[1;37m"; // BOLD WHITE
+
+    // Background
+    public static final String BLACK_BACKGROUND = "\u001B[40m";  // BLACK
+    public static final String RED_BACKGROUND = "\u001B[41m";    // RED
+    public static final String GREEN_BACKGROUND = "\u001B[42m";  // GREEN
+    public static final String YELLOW_BACKGROUND = "\u001B[43m"; // YELLOW
+    public static final String BLUE_BACKGROUND = "\u001B[44m";   // BLUE
+    public static final String PURPLE_BACKGROUND = "\u001B[45m"; // PURPLE
+    public static final String CYAN_BACKGROUND = "\u001B[46m";   // CYAN
+    public static final String WHITE_BACKGROUND = "\u001B[47m";  // WHITE
+
+    // High Intensity (Bright colors)
+    public static final String BLACK_BRIGHT = "\u001B[90m";  // BRIGHT BLACK
+    public static final String RED_BRIGHT = "\u001B[91m";    // BRIGHT RED
+    public static final String GREEN_BRIGHT = "\u001B[92m";  // BRIGHT GREEN
+    public static final String YELLOW_BRIGHT = "\u001B[93m"; // BRIGHT YELLOW
+    public static final String BLUE_BRIGHT = "\u001B[94m";   // BRIGHT BLUE
+    public static final String PURPLE_BRIGHT = "\u001B[95m"; // BRIGHT PURPLE
+    public static final String CYAN_BRIGHT = "\u001B[96m";   // BRIGHT CYAN
+    public static final String WHITE_BRIGHT = "\u001B[97m";  // BRIGHT WHITE
+
+    // Bold High Intensity
+    public static final String BLACK_BOLD_BRIGHT = "\u001B[1;90m"; // BOLD BRIGHT BLACK
+    public static final String RED_BOLD_BRIGHT = "\u001B[1;91m";   // BOLD BRIGHT RED
+    public static final String GREEN_BOLD_BRIGHT = "\u001B[1;92m"; // BOLD BRIGHT GREEN
+    public static final String YELLOW_BOLD_BRIGHT = "\u001B[1;93m";// BOLD BRIGHT YELLOW
+    public static final String BLUE_BOLD_BRIGHT = "\u001B[1;94m";  // BOLD BRIGHT BLUE
+    public static final String PURPLE_BOLD_BRIGHT = "\u001B[1;95m";// BOLD BRIGHT PURPLE
+    public static final String CYAN_BOLD_BRIGHT = "\u001B[1;96m";  // BOLD BRIGHT CYAN
+    public static final String WHITE_BOLD_BRIGHT = "\u001B[1;97m"; // BOLD BRIGHT WHITE
+
+}
